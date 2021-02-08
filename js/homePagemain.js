bffb6b5ee634273e9dc0b5c0286eb863f9954375
Hamza ZAOUYA <61616479+hamzazaouya@users.noreply.github.com>
2021-02-08 12:19:12 +0100

    final the responsive nav bar withowt dropdown

---
+++ js/homePagemain.js
@@ -0,0 +1,23 @@
+/*global document, window*/
+
+//Start Hamburgur Icon
+var humIcon = document.getElementById('hamburger');
+var times = document.getElementById('times');
+window.onload = times.classList.remove('times');
+//End Hamburgur Icon
+
+//Start Download Button.
+var downloadBtn = document.getElementById('onload');
+window.onload = downloadBtn.classList.add("onload");
+//End Download Button.
+
+//Start Show And Hide Download and Nav Links Sections.
+var links = document.getElementById('links');
+var downloads = document.getElementById('download');
+humIcon.onclick = function () {
+    'use strict';
+    links.classList.toggle('show');
+    downloads.classList.toggle('show');
+    times.classList.toggle('times');
+};
+//End Show And Hide Download and Nav Links Sections.
